565e364be5fb46437329fb5522e19a0a56d1814a
shraddha <shraddhayeole15@gmail.com>
2017-12-10 21:22:14 -0800

    Changed Train Controller for search train

--- ticketbooking/src/main/java/com/example/ticketbooking/controller/TrainController.java
+++ ticketbooking/src/main/java/com/example/ticketbooking/controller/TrainController.java
@@ -1,5 +1,7 @@
 package com.example.ticketbooking.controller;
 
+import java.util.Date;
+
 import javax.transaction.Transactional;
 
 
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -23,11 +26,24 @@ public class TrainController {
 	@Autowired
 	private TrainService trainService;
 	
-	@RequestMapping(value = "/train/{id}", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	    public ResponseEntity<?> searchTrain(@PathVariable Long id) {
+	@RequestMapping(value = "/train/search", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	    public ResponseEntity<?> searchTrain(
+	    		//@PathVariable Long id
+	    		@RequestParam(value="email",required=true) String email,
+	    		@RequestParam(value="departuredate",required=true) Date departureDate,
+	    		@RequestParam(value="departuretime",required=true) String departureTime,
+	    		@RequestParam(value="arrivaltime",required=true) String arrivalTime,
+	    		@RequestParam(value="fromstation",required=true) String fromStation,
+	    		@RequestParam(value="tostation",required=true) String toStation,
+	    		@RequestParam(value="noofconnections") int noOfConnections,
+	    		@RequestParam(value="nooftickets") int noOfTickets
+	    		
+	    		
+	    		) {
 		 System.out.println("Inside GET Request");
 	    	
-	        return trainService.searchForTrain(id);
+	      //  return trainService.searchForTrain(id);
+		 return trainService.searchForTrain(email,departureDate, departureTime, arrivalTime, fromStation, toStation, noOfConnections, noOfTickets);
 	    }
 
 }
